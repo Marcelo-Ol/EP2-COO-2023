47053980bd16d4ae05639ff601dcd51911d0c696
WillDevGit <williansilvaws063@gmail.com>
2023-07-22 15:23:34 -0300

    Inversão dos nomes e ArrayList

--- GeradorDeRelatorios.java
+++ GeradorDeRelatorios.java
@@ -1,12 +1,12 @@
 import java.io.PrintWriter;
 import java.io.IOException;
-
+import java.util.ArrayList; 
 import java.util.*;
 
 public class GeradorDeRelatorios {
 
-	public static final String ALG_INSERTIONSORT = "quick";
-	public static final String ALG_QUICKSORT = "insertion";
+	public static final String ALG_INSERTIONSORT = "insertion";
+	public static final String ALG_QUICKSORT = "quick";
 
 	public static final String CRIT_DESC_CRESC = "descricao_c";
 	public static final String CRIT_PRECO_CRESC = "preco_c";
@@ -22,161 +22,126 @@ public class GeradorDeRelatorios {
 	public static final int FORMATO_NEGRITO = 0b0001;
 	public static final int FORMATO_ITALICO = 0b0010;
 
-	private Produto [] produtos;
+	private ArrayList<Produto> produtos;
 	private String algoritmo;
 	private String criterio;
 	private String filtro;
 	private String argFiltro;
 	private int format_flags;	
 
-	public GeradorDeRelatorios(Produto [] produtos, String algoritmo, String criterio, String filtro, String argFiltro, int format_flags){
+	public GeradorDeRelatorios(ArrayList<Produto> produtos, String algoritmo, String criterio, String filtro, String argFiltro, int format_flags){
 
-		this.produtos = new Produto[produtos.length];
-		
-		for(int i = 0; i < produtos.length; i++){
-		
-			this.produtos[i] = produtos[i];
-		}
-
-		this.algoritmo = algoritmo;
-		this.criterio = criterio;
-		this.format_flags = format_flags;
-		this.filtro = filtro;
-		this.argFiltro = argFiltro;
+		this.produtos = new ArrayList<>(produtos);
+    	this.algoritmo = algoritmo;
+    	this.criterio = criterio;
+    	this.format_flags = format_flags;
+    	this.filtro = filtro;
+    	this.argFiltro = argFiltro;
 	}
-	//testando
-	private int particiona(int ini, int fim){
-
-		Produto x = produtos[ini];
-		int i = (ini - 1);
-		int j = (fim + 1);
-
-		while(true){
-
-			if(criterio.equals(CRIT_DESC_CRESC)){
-
-				do{ 
+	
+	private int particiona(int ini, int fim) {
+		Produto x = produtos.get(ini);
+		int i = ini - 1;
+		int j = fim + 1;
+	
+		while (true) {
+			if (criterio.equals(CRIT_DESC_CRESC)) {
+				do {
 					j--;
-
-				} while(produtos[j].getDescricao().compareToIgnoreCase(x.getDescricao()) > 0);
-			
-				do{
+				} while (produtos.get(j).getDescricao().compareToIgnoreCase(x.getDescricao()) > 0);
+	
+				do {
 					i++;
-
-				} while(produtos[i].getDescricao().compareToIgnoreCase(x.getDescricao()) < 0);
-			}
-			else if(criterio.equals(CRIT_PRECO_CRESC)){
-
-				do{ 
+				} while (produtos.get(i).getDescricao().compareToIgnoreCase(x.getDescricao()) < 0);
+			} else if (criterio.equals(CRIT_PRECO_CRESC)) {
+				do {
 					j--;
-
-				} while(produtos[j].getPreco() > x.getPreco());
-			
-				do{
+				} while (produtos.get(j).getPreco() > x.getPreco());
+	
+				do {
 					i++;
-
-				} while(produtos[i].getPreco() < x.getPreco());
-			}
-
-			else if(criterio.equals(CRIT_ESTOQUE_CRESC)){
-
-				do{ 
+				} while (produtos.get(i).getPreco() < x.getPreco());
+			} else if (criterio.equals(CRIT_ESTOQUE_CRESC)) {
+				do {
 					j--;
-
-				} while(produtos[j].getQtdEstoque() > x.getQtdEstoque());
-			
-				do{
+				} while (produtos.get(j).getQtdEstoque() > x.getQtdEstoque());
+	
+				do {
 					i++;
-
-				} while(produtos[i].getQtdEstoque() < x.getQtdEstoque());
-
-			}
-			else{
-
+				} while (produtos.get(i).getQtdEstoque() < x.getQtdEstoque());
+			} else {
 				throw new RuntimeException("Criterio invalido!");
 			}
-
-			if(i < j){
-				Produto temp = produtos[i];
-				produtos[i] = produtos[j]; 				
-				produtos[j] = temp;
+	
+			if (i < j) {
+				Produto temp = produtos.get(i);
+				produtos.set(i, produtos.get(j));
+				produtos.set(j, temp);
+			} else {
+				return j;
 			}
-			else return j;
 		}
 	}
-
-	private void ordena(int ini, int fim){
-
-		if(algoritmo.equals(ALG_INSERTIONSORT)){
-
-			for(int i = ini; i <= fim; i++){
-
-				Produto x = produtos[i];				
-				int j = (i - 1);
-
-				while(j >= ini){
-
-					if(criterio.equals(CRIT_DESC_CRESC)){
-
-						if( x.getDescricao().compareToIgnoreCase(produtos[j].getDescricao()) < 0 ){
-			
-							produtos[j + 1] = produtos[j];
+	
+	private void ordena(int ini, int fim) {
+		if (algoritmo.equals(ALG_INSERTIONSORT)) {
+			for (int i = ini; i <= fim; i++) {
+				Produto x = produtos.get(i);
+				int j = i - 1;
+	
+				while (j >= ini) {
+					if (criterio.equals(CRIT_DESC_CRESC)) {
+						if (x.getDescricao().compareToIgnoreCase(produtos.get(j).getDescricao()) < 0) {
+							produtos.set(j + 1, produtos.get(j));
 							j--;
+						} else {
+							break;
 						}
-						else break;
-					}
-					else if(criterio.equals(CRIT_PRECO_CRESC)){
-
-						if(x.getPreco() < produtos[j].getPreco()){
-			
-							produtos[j + 1] = produtos[j];
+					} else if (criterio.equals(CRIT_PRECO_CRESC)) {
+						if (x.getPreco() < produtos.get(j).getPreco()) {
+							produtos.set(j + 1, produtos.get(j));
 							j--;
+						} else {
+							break;
 						}
-						else break;
-					}
-					else if(criterio.equals(CRIT_ESTOQUE_CRESC)){
-
-						if(x.getQtdEstoque() < produtos[j].getQtdEstoque()){
-			
-							produtos[j + 1] = produtos[j];
+					} else if (criterio.equals(CRIT_ESTOQUE_CRESC)) {
+						if (x.getQtdEstoque() < produtos.get(j).getQtdEstoque()) {
+							produtos.set(j + 1, produtos.get(j));
 							j--;
+						} else {
+							break;
 						}
-						else break;
+					} else {
+						throw new RuntimeException("Criterio invalido!");
 					}
-					else throw new RuntimeException("Criterio invalido!");
 				}
-
-				produtos[j + 1] = x;
+	
+				produtos.set(j + 1, x);
 			}
-		}
-		else if(algoritmo.equals(ALG_QUICKSORT)){
-
-			if(ini < fim) {
-
+		} else if (algoritmo.equals(ALG_QUICKSORT)) {
+			if (ini < fim) {
 				int q = particiona(ini, fim);
-				
+	
 				ordena(ini, q);
 				ordena(q + 1, fim);
 			}
-		}
-		else {
+		} else {
 			throw new RuntimeException("Algoritmo invalido!");
 		}
 	}
 	
 	
-	public void debug(){
-
-		System.out.println("Gerando relatório para array contendo " + produtos.length + " produto(s)");
-		System.out.println("parametro filtro = '" + argFiltro + "'"); 
+	public void debug() {
+		System.out.println("Gerando relatório para ArrayList contendo " + produtos.size() + " produto(s)");
+		System.out.println("parametro filtro = '" + argFiltro + "'");
 	}
-
+	
 
 	public void geraRelatorio(String arquivoSaida) throws IOException {
 
 		debug();
 
-		ordena(0, produtos.length - 1);
+		ordena(0, produtos.size() - 1);
 
 		PrintWriter out = new PrintWriter(arquivoSaida);
 
@@ -188,10 +153,10 @@ public class GeradorDeRelatorios {
 
 		int count = 0;
 
-		for(int i = 0; i < produtos.length; i++){
 
-			Produto p = produtos[i];
-			boolean selecionado = false;
+    for (int i = 0; i < produtos.size(); i++) {
+        Produto p = produtos.get(i);
+        boolean selecionado = false;
 
 			if(filtro.equals(FILTRO_TODOS)){
 
@@ -241,51 +206,51 @@ public class GeradorDeRelatorios {
 		}
 
 		out.println("</ul>");
-		out.println(count + " produtos listados, de um total de " + produtos.length + ".");
+		out.println(count + " produtos listados, de um total de " + produtos.size() + ".");
 		out.println("</body>");
 		out.println("</html>");
 
 		out.close();
 	}
 
-	public static Produto [] carregaProdutos(){
-
-		return new Produto [] { 
-
-			new ProdutoPadrao( 1, "O Hobbit", "Livros", 2, 34.90),
-			new ProdutoPadrao( 2, "Notebook Core i7", "Informatica", 5, 1999.90),
-			new ProdutoPadrao( 3, "Resident Evil 4", "Games", 7, 79.90),
-			new ProdutoPadrao( 4, "iPhone", "Telefonia", 8, 4999.90),
-			new ProdutoPadrao( 5, "Calculo I", "Livros", 20, 55.00),
-			new ProdutoPadrao( 6, "Power Glove", "Games", 3, 499.90),
-			new ProdutoPadrao( 7, "Microsoft HoloLens", "Informatica", 1, 19900.00),
-			new ProdutoPadrao( 8, "OpenGL Programming Guide", "Livros", 4, 89.90),
-			new ProdutoPadrao( 9, "Vectrex", "Games", 1, 799.90),
-			new ProdutoPadrao(10, "Carregador iPhone", "Telefonia", 15, 499.90),
-			new ProdutoPadrao(11, "Introduction to Algorithms", "Livros", 7, 315.00),
-			new ProdutoPadrao(12, "Daytona USA (Arcade)", "Games", 1, 12000.00),
-			new ProdutoPadrao(13, "Neuromancer", "Livros", 5, 45.00),
-			new ProdutoPadrao(14, "Nokia 3100", "Telefonia", 4, 249.99),
-			new ProdutoPadrao(15, "Oculus Rift", "Games", 1, 3600.00),
-			new ProdutoPadrao(16, "Trackball Logitech", "Informatica", 1, 250.00),
-			new ProdutoPadrao(17, "After Burner II (Arcade)", "Games", 2, 8900.0),
-			new ProdutoPadrao(18, "Assembly for Dummies", "Livros", 30, 129.90),
-			new ProdutoPadrao(19, "iPhone (usado)", "Telefonia", 3, 3999.90),
-			new ProdutoPadrao(20, "Game Programming Patterns", "Livros", 1, 299.90),
-			new ProdutoPadrao(21, "Playstation 2", "Games", 10, 499.90),
-			new ProdutoPadrao(22, "Carregador Nokia", "Telefonia", 14, 89.00),
-			new ProdutoPadrao(23, "Placa Aceleradora Voodoo 2", "Informatica", 4, 189.00),
-			new ProdutoPadrao(24, "Stunts", "Games", 3, 19.90),
-			new ProdutoPadrao(25, "Carregador Generico", "Telefonia", 9, 30.00),
-			new ProdutoPadrao(26, "Monitor VGA 14 polegadas", "Informatica", 2, 199.90),
-			new ProdutoPadrao(27, "Nokia N-Gage", "Telefonia", 9, 699.00),
-			new ProdutoPadrao(28, "Disquetes Maxell 5.25 polegadas (caixa com 10 unidades)", "Informatica", 23, 49.00),
-			new ProdutoPadrao(29, "Alone in The Dark", "Games", 11, 59.00),
-			new ProdutoPadrao(30, "The Art of Computer Programming Vol. 1", "Livros", 3, 240.00),
-			new ProdutoPadrao(31, "The Art of Computer Programming Vol. 2", "Livros", 2, 200.00),
-			new ProdutoPadrao(32, "The Art of Computer Programming Vol. 3", "Livros", 4, 270.00)
-		};
-	} 
+	public static ArrayList<Produto> carregaProdutos() {
+		ArrayList<Produto> produtos = new ArrayList<>();
+	
+		produtos.add(new ProdutoPadrao(1, "O Hobbit", "Livros", 2, 34.90));
+		produtos.add(new ProdutoPadrao(2, "Notebook Core i7", "Informatica", 5, 1999.90));
+		produtos.add(new ProdutoPadrao(3, "Resident Evil 4", "Games", 7, 79.90));
+		produtos.add(new ProdutoPadrao(4, "iPhone", "Telefonia", 8, 4999.90));
+		produtos.add(new ProdutoPadrao(5, "Calculo I", "Livros", 20, 55.00));
+		produtos.add(new ProdutoPadrao(6, "Power Glove", "Games", 3, 499.90));
+		produtos.add(new ProdutoPadrao(7, "Microsoft HoloLens", "Informatica", 1, 19900.00));
+		produtos.add(new ProdutoPadrao(8, "OpenGL Programming Guide", "Livros", 4, 89.90));
+		produtos.add(new ProdutoPadrao(9, "Vectrex", "Games", 1, 799.90));
+		produtos.add(new ProdutoPadrao(10, "Carregador iPhone", "Telefonia", 15, 499.90));
+		produtos.add(new ProdutoPadrao(11, "Introduction to Algorithms", "Livros", 7, 315.00));
+		produtos.add(new ProdutoPadrao(12, "Daytona USA (Arcade)", "Games", 1, 12000.00));
+		produtos.add(new ProdutoPadrao(13, "Neuromancer", "Livros", 5, 45.00));
+		produtos.add(new ProdutoPadrao(14, "Nokia 3100", "Telefonia", 4, 249.99));
+		produtos.add(new ProdutoPadrao(15, "Oculus Rift", "Games", 1, 3600.00));
+		produtos.add(new ProdutoPadrao(16, "Trackball Logitech", "Informatica", 1, 250.00));
+		produtos.add(new ProdutoPadrao(17, "After Burner II (Arcade)", "Games", 2, 8900.0));
+		produtos.add(new ProdutoPadrao(18, "Assembly for Dummies", "Livros", 30, 129.90));
+		produtos.add(new ProdutoPadrao(19, "iPhone (usado)", "Telefonia", 3, 3999.90));
+		produtos.add(new ProdutoPadrao(20, "Game Programming Patterns", "Livros", 1, 299.90));
+		produtos.add(new ProdutoPadrao(21, "Playstation 2", "Games", 10, 499.90));
+		produtos.add(new ProdutoPadrao(22, "Carregador Nokia", "Telefonia", 14, 89.00));
+		produtos.add(new ProdutoPadrao(23, "Placa Aceleradora Voodoo 2", "Informatica", 4, 189.00));
+		produtos.add(new ProdutoPadrao(24, "Stunts", "Games", 3, 19.90));
+		produtos.add(new ProdutoPadrao(25, "Carregador Generico", "Telefonia", 9, 30.00));
+		produtos.add(new ProdutoPadrao(26, "Monitor VGA 14 polegadas", "Informatica", 2, 199.90));
+		produtos.add(new ProdutoPadrao(27, "Nokia N-Gage", "Telefonia", 9, 699.00));
+		produtos.add(new ProdutoPadrao(28, "Disquetes Maxell 5.25 polegadas (caixa com 10 unidades)", "Informatica", 23, 49.00));
+		produtos.add(new ProdutoPadrao(29, "Alone in The Dark", "Games", 11, 59.00));
+		produtos.add(new ProdutoPadrao(30, "The Art of Computer Programming Vol. 1", "Livros", 3, 240.00));
+		produtos.add(new ProdutoPadrao(31, "The Art of Computer Programming Vol. 2", "Livros", 2, 200.00));
+		produtos.add(new ProdutoPadrao(32, "The Art of Computer Programming Vol. 3", "Livros", 4, 270.00));
+	
+		return produtos;
+	}
 
 	public static void main(String [] args) {
 
