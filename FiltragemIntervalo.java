78087db2deafd2b7810488a669de38a9c75a9cd1
WillDevGit <williansilvaws063@gmail.com>
2023-07-23 17:23:12 -0300

    Adiciona novos filtros

--- FiltragemIntervalo.java
+++ FiltragemIntervalo.java
@@ -2,7 +2,7 @@ public class FiltragemIntervalo implements FiltragemStrategy {
     
     @Override
     public boolean verificar(Produto produto, String filtro, String argFiltro) {
-        return produto.getPreco() == Integer.parseInt(argFiltro);
+        return produto.getPreco() > Integer.parseInt(argFiltro);
     }
 
 }
--- FiltragemSubString.java
+++ FiltragemSubString.java
@@ -2,7 +2,6 @@ public class FiltragemSubString implements FiltragemStrategy {
     
     @Override
     public boolean verificar(Produto produto, String filtro, String argFiltro) {
-        double precoProduto = produto.getPreco();
-        return precoProduto >= precoMinimo && precoProduto <= precoMaximo;
+        return false;
     }
 }
