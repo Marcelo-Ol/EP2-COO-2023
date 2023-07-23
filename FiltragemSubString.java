0df7e8f6d7f403cae95eaf305f53764e0286ee65
WillDevGit <williansilvaws063@gmail.com>
2023-07-23 18:22:26 -0300

    Adiciona Case insensitive

--- FiltragemSubString.java
+++ FiltragemSubString.java
@@ -2,6 +2,8 @@ public class FiltragemSubString implements FiltragemStrategy {
     
     @Override
     public boolean verificar(Produto produto, String filtro, String argFiltro) {
-        return produto.getDescricao().contains(argFiltro);
+        String descricaoProduto = produto.getDescricao().toLowerCase();
+        String argFiltroLowerCase = argFiltro.toLowerCase();
+        return descricaoProduto.contains(argFiltroLowerCase);
     }
 }
