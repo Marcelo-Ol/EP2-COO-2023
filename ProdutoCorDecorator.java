1d778732e6f7526402259a100f1fc5edd02e36e2
Guilherme Fabrício de Oliveira <guifabricio2004@gmail.com>
2023-07-23 23:08:57 -0300

    Update ProdutoCorDecorator.java
    Eu acho q assim funciona, mas na hora de criar o objeto da classe da problema por causa do construtor

--- ProdutoCorDecorator.java
+++ ProdutoCorDecorator.java
@@ -1,10 +1,62 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ProdutoCorDecorator extends ProdutoDecorator {
+    private String cor;
+
     public ProdutoCorDecorator(Produto produto) {
         super(produto);
     }
 
     @Override
     public String formataParaImpressao() {
-        return "<span style=\"color:red\">" + produto.formataParaImpressao() + "</span>";
+        return "<span style=\"color" + cor + "\">" + produto.formataParaImpressao() + "</span>";
+    }
+
+    public static void main(String[] args){
+        ProdutoCorDecorator obj = new ProdutoCorDecorator(produto);
+        obj.run();
+    }
+
+    public void run(){
+        String arquivoCSV = "produtos.csv";
+        BufferedReader br = null;
+        String linha = "";
+        String csvDivisor = ",";
+
+        try{
+
+            br = new BufferedReader(new FileReader(arquivoCSV));
+
+            while ((linha = br.readLine()) != null){
+                
+                String[] produto = linha.split(csvDivisor);
+
+                cor = produto[produto.length-1];
+            }
+
+        } catch (FileNotFoundException e){
+
+            e.printStackTrace();
+
+        } catch (IOException e){
+
+            e.printStackTrace();
+
+        } finally {
+
+            if(br != null){
+
+                try{
+                    
+                    br.close();
+                } catch (IOException e){
+
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
